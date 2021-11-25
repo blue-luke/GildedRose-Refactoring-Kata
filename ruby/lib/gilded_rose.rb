9bfdbac6a0b8d0aa846c97e3a9e3f29935a844e4
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2021-11-25 16:28:27 +0000

    Removal of most magic numbers; creation of item_dummy to isolate GildedRose tests

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -1,24 +1,32 @@
+require './lib/item'
+
 class GildedRose
 
   def initialize(items)
     @items = items
+    @time_period = 1
+    @regular_degrade = 1
+    @advanced_degrade = @regular_degrade * 2
+    @conjured_degrade = @regular_degrade * 2
+    @conjured_advanced_degrade = @advanced_degrade * 2
+    @lowest_quality = 0
+    @highest_quality = 50
   end
 
   def update()
     @items.each do |item|
-      update_quality(item)
       update_sell_in(item)
+      update_quality(item)
     end
   end
 
   def update_sell_in(item)
-    if item.name != "Sulfuras, Hand of Ragnaros"
-      item.sell_in = item.sell_in - 1
+    if !item.name.include?("Sulfuras")
+      item.sell_in -= @time_period
     end
   end
 
   def update_quality(item)
-
     if item.name.include?("Brie")
       change_brie_quality(item)
     elsif item.name.include?("Conjured")
@@ -29,60 +37,50 @@ class GildedRose
       return
     else change_regular_item_quality(item)
     end
-
   end
 
   def change_brie_quality(brie)
-    if brie.quality < 50
+    if brie.quality < @highest_quality
       brie.quality += 1
     end
   end
 
   def change_backstage_pass_quality(backstage_pass)
-    case backstage_pass.sell_in
-    when backstage_pass.sell_in >= 11
+    if backstage_pass.sell_in >= 11
       backstage_pass.quality += 1
-    when backstage_pass.sell_in <= 10 && backstage_pass.sell_in >=6
+    elsif backstage_pass.sell_in <= 10 && backstage_pass.sell_in >= 6
       backstage_pass.quality += 2
-    when backstage_pass.sell_in <= 5
+    elsif backstage_pass.sell_in <= 5 && backstage_pass.sell_in >= 0
       backstage_pass.quality += 3
+    elsif backstage_pass.sell_in < 0
+      backstage_pass.quality = 0
     end
     check_quality_range(backstage_pass.quality)
   end
 
   def change_conjured_item_quality(conjured_item)
-    if conjured_item.quality > 0
-      conjured_item.quality -= 2
+    if conjured_item.sell_in >= @lowest_quality
+      conjured_item.quality -= @conjured_degrade
+    elsif conjured_item.sell_in < @lowest_quality
+      conjured_item.quality -= @conjured_advanced_degrade
     end
     check_quality_range(conjured_item.quality)
   end
 
   def change_regular_item_quality(regular_item)
-    if regular_item.quality > 0
-      regular_item.quality -= 1
+    if regular_item.sell_in >= @lowest_quality
+      regular_item.quality -= @regular_degrade
+    elsif regular_item.sell_in < @lowest_quality
+      regular_item.quality -= @advanced_degrade
     end
   end
 
   def check_quality_range(quality)
-    if quality < 0
-      quality = 0
-    elsif quality > 50
-      quality = 50
+    if quality < @lowest_quality
+      quality = @lowest_quality
+    elsif quality > @highest_quality
+      quality = @highest_quality
     end
   end
 
-end
-
-class Item
-  attr_accessor :name, :sell_in, :quality
-
-  def initialize(name, sell_in, quality)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
-  end
-
-  def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
-  end
 end
--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -1,20 +1,86 @@
 require './lib/gilded_rose'
+require './dummies/item_dummy'
 
 describe GildedRose do
   describe '#update' do
-    it 'reduces conjured item quality by 2' do
-      item9 = Item.new('Conjured Mana Cake', 3, 6)
-      inn1 = GildedRose.new([item9])
-      inn1.update
-      expect(item9.sell_in).to eq(2)
-      expect(item9.quality).to eq(4)
+
+    describe 'regular items' do
+      it 'decreases quality by 1, decreases sell in by 1 when sell in 0 or more' do
+        item1 = ItemDummy.new('+5 Dexterity Vest', 10, 20)
+        inn1 = GildedRose.new([item1])
+        inn1.update
+        expect(item1.sell_in).to eq(9)
+        expect(item1.quality).to eq(19)
+      end
+      it 'decreases quality by 2, decreases sell in by 1 when sell in less than 0' do
+        item1 = ItemDummy.new('+5 Dexterity Vest', -1, 20)
+        inn1 = GildedRose.new([item1])
+        inn1.update
+        expect(item1.sell_in).to eq(-2)
+        expect(item1.quality).to eq(18)
+      end
     end
-    it 'reduces conjured item quality by 2' do
-      item9 = Item.new('Conjured Mana Cake', 3, 6)
-      inn1 = GildedRose.new([item9])
-      inn1.update
-      expect(item9.sell_in).to eq(2)
-      expect(item9.quality).to eq(4)
+
+    describe 'brie' do
+      it 'increases quality by 1, decreases sell in by 1 ' do
+        item2 = ItemDummy.new('Aged Brie', 2, 0)
+        inn1 = GildedRose.new([item2])
+        inn1.update
+        expect(item2.sell_in).to eq(1)
+        expect(item2.quality).to eq(1)
+      end
+      it 'quality of an item never exceeds 50, except sulfuras' do
+        item2 = ItemDummy.new('Aged Brie', 2, 50)
+        inn1 = GildedRose.new([item2])
+        inn1.update
+        expect(item2.sell_in).to eq(1)
+        expect(item2.quality).to eq(50)
+      end
     end
+
+    describe 'sulfuras' do
+      it 'keeps quality the same, keeps sell in the same' do
+        item4 = ItemDummy.new('Sulfuras, Hand of Ragnaros', 0, 80)
+        inn1 = GildedRose.new([item4])
+        inn1.update
+        expect(item4.sell_in).to eq(0)
+        expect(item4.quality).to eq(80)
+      end
+    end
+
+    describe 'backstage passes' do
+      it 'increases quality by 1, decreases sell in by 1' do
+        item6 = ItemDummy.new('Backstage passes to a TAFKAL80ETC concert', 15, 20)
+        inn1 = GildedRose.new([item6])
+        inn1.update
+        expect(item6.sell_in).to eq(14)
+        expect(item6.quality).to eq(21)
+      end
+      it 'quality drops to 0 when sell in less than 0' do
+        item6 = ItemDummy.new('Backstage passes to a TAFKAL80ETC concert', 0, 50)
+        inn1 = GildedRose.new([item6])
+        inn1.update
+        expect(item6.sell_in).to eq(-1)
+        expect(item6.quality).to eq(0)
+      end
+    end
+
+    describe 'conjured items' do
+      it 'reduces quality by 2, decreases sell in by 1 when sell in 0 or more' do
+        item9 = ItemDummy.new('Conjured Mana Cake', 3, 6)
+        inn1 = GildedRose.new([item9])
+        inn1.update
+        expect(item9.sell_in).to eq(2)
+        expect(item9.quality).to eq(4)
+      end
+      it 'reduces quality by 4, decreases sell in by 1 when sell in less than 0' do
+        item9 = ItemDummy.new('Conjured Mana Cake', 0, 6)
+        inn1 = GildedRose.new([item9])
+        inn1.update
+        expect(item9.sell_in).to eq(-1)
+        expect(item9.quality).to eq(2)
+      end
+    end
+
   end
 end
