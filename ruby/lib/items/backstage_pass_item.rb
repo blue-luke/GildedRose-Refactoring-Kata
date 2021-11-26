c015cb60b1d06ce0d9daefda5967ef2ff5fba1b4
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2021-11-26 09:30:27 +0000

    Reverse changes to item class but kept sub-classes

--- ruby/lib/items/backstage_pass_item.rb
+++ ruby/lib/items/backstage_pass_item.rb
@@ -3,6 +3,38 @@
 require_relative '../item'
 
 class BackstagePassItem < Item
+
+  def initialize(name, sell_in, quality)
+    @name = name
+    @sell_in = sell_in
+    @quality = quality
+    @min_quality = 0
+    @max_quality = 50
+    @regular_degrade = 1
+    @advanced_degrade = @regular_degrade * 2
+  end
+
+  def to_s
+    "#{@name}, #{@sell_in}, #{@quality}"
+  end
+
+  def check_quality_range(_quality)
+    if @quality < @min_quality
+      @quality = @min_quality
+    elsif @quality > @max_quality
+      @quality = @max_quality
+    end
+  end
+
+  def update
+    update_sell_in
+    update_quality
+  end
+
+  def update_sell_in
+    @sell_in -= 1
+  end
+
   def update_quality
     if @sell_in >= 11
       @quality += 1
--- ruby/lib/items/legendary_item.rb
+++ ruby/lib/items/legendary_item.rb
@@ -3,6 +3,12 @@
 require_relative '../item'
 
 class LegendaryItem < Item
+
+  def update
+    update_sell_in
+    update_quality
+  end
+
   def update_sell_in
     nil
   end
